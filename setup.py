6ba2fe672bad3c27a9211526d38f8e92a8fd689f
abhiram <abhiramtamarapalli2@gmail.com>
2025-06-07 12:58:42 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='nanohealth-futures',
+    version='0.0.0',
+    author='abhiram tamarapalli',
+    author_email='abhiramtamarapalli2@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
